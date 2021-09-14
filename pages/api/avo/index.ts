4d4e11b46ff8e9c55978a4e5d486a98c01ac23d1
juanmalob <juanmalobato@gmail.com>
2021-09-14 08:43:48 +0200

    Segundo commmit

---
+++ pages/api/avo/index.ts
@@ -0,0 +1,10 @@
+import { IncomingMessage, ServerResponse } from "http"
+
+const allAvos = async (request: IncomingMessage, response: ServerResponse) => {
+
+    response.end(JSON.stringify({ hello:'world'}))
+    
+
+}
+
+export default allAvos 
